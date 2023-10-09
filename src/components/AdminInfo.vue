5dff67d023a9ac3967ddc4636b4e6be32a013715
Mike Storey <mike@ssgpp.com>
2023-10-08 21:14:47 -0400

    Added config items to admin page

--- src/components/AdminInfo.vue
+++ src/components/AdminInfo.vue
@@ -1,10 +1,26 @@
 <template>
-    <div>
-        <h1>Admin Dashboard</h1>
-        <v-text-field label="Database Version" v-model="databaseVersion"></v-text-field>
-        <v-text-field label="API Version" v-model="apiVersion"></v-text-field>
-        <v-text-field label="UI Version" v-model="uiVersion"></v-text-field>
+  <div>
+    <h1>Admin Dashboard</h1>
+    <v-text-field label="Database Version" v-model="databaseVersion"></v-text-field>
+    <v-text-field label="API Version" v-model="apiVersion"></v-text-field>
+    <v-text-field label="UI Version" v-model="uiVersion"></v-text-field>
+    <h2>Configuration Items</h2>
+    <div class="flex-container">
+      <ul>
+        <li v-for="item in configItems" :key="item.name" class="flex-item">
+          <div class="flex-subitem">
+            {{ item.name }}
+          </div>
+          <div class="flex-subitem">
+            {{ item.value }}
+          </div>
+          <div class="flex-subitem">
+            From: {{ item.from }}
+          </div>
+        </li>
+      </ul>
     </div>
+  </div>
 </template>
   
 <script>
@@ -15,7 +31,8 @@ export default {
     return {
         databaseVersion: '',
         apiVersion: '',
-        uiVersion: ''
+        uiVersion: '',
+        configItems: []  // New property to hold ConfigItems
     };
   },
   mounted() {
@@ -29,12 +46,10 @@ export default {
 
       axios.get(apiUrl)
         .then(response => {
-          console.log("response:", response);
-          console.log("response.data.DBVersion:", response.data.DBVersion);
           this.databaseVersion = response.data.DBVersion;
-          console.log("this.databaseVersion:", this.databaseVersion);
           this.apiVersion = response.data.Version;
           this.uiVersion = '1.0.0'; // Your UI version
+          this.configItems = response.data.ConfigItems
         })
         .catch(error => {
           console.log("Error:", error);
@@ -44,3 +59,34 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+
+ul {
+  padding: 0;
+  margin: 0;
+  width: 100%;
+}
+.flex-container {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 100%;
+}
+
+.flex-item {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  margin: 5px;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+.flex-subitem {
+  flex: 1;
+  text-align: left;
+  width: 33%;
+}
+</style>

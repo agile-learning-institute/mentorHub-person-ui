8e754749ad3ccccbf449ef66c07eb5b86238aae1
Mike Storey <mike@ssgpp.com>
2023-10-31 10:09:12 -0400

    local docker build cleanup

--- src/docker/docker-build.sh
+++ src/docker/docker-build.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-cd ../..
 npm install
 npm run build
 BRANCH=$(git branch --show-current)
 PATCH=$(git rev-parse $BRANCH)
 echo $BRANCH.$PATCH > ./dist/patch.txt
-cd src/docker
 docker build . --tag ghcr.io/agile-learning-institute/institute-person-ui:latest
-# docker tag institute-person-ui:latest ghcr.io/agile-learning-institute/institute-person-ui:latest
 # docker push ghcr.io/agile-learning-institute/institute-person-ui:latest

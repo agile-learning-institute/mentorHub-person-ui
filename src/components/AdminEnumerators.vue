17e8a5d1fb5b9746d4a6f7674f0102c4d16a26c9
Mike Storey <mike@ssgpp.com>
2023-10-31 09:27:16 -0400

    Updates to status / statuses enums data

--- src/components/AdminEnumerators.vue
+++ src/components/AdminEnumerators.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <EnumeratorItem :enumerators="config.enums.status" :name="'Status'"/>
+    <EnumeratorItem :enumerators="config.enums.statuses" :name="'Status'"/>
     <EnumeratorItem :enumerators="config.enums.device" :name="'Devices'"/>
     <EnumeratorItem :enumerators="config.enums.title" :name="'Titles'"/>
     <EnumeratorItem :enumerators="config.enums.domain" :name="'Domains'"/>

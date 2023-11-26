d75092250a8e7d6edc17a308702001b3b193e82c
Mike Storey <mike@ssgpp.com>
2023-11-26 16:52:28 -0500

    mentorhub rename

--- src/docker/docker-build.sh
+++ src/docker/docker-build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Build Docker Image
-docker build --file src/docker/Dockerfile --tag ghcr.io/agile-learning-institute/institute-person-ui:latest .
+docker build --file src/docker/Dockerfile --tag ghcr.io/agile-learning-institute/mentorhub-person-ui:latest .
 if [ $? -ne 0 ]; then
     echo "Docker build failed"
     exit 1
 fi
 
-curl https://raw.githubusercontent.com/agile-learning-institute/institute/main/docker-compose/run-local-person.sh | /bin/bash
+curl https://raw.githubusercontent.com/agile-learning-institute/institute/mentorhub/docker-compose/run-local-person.sh | /bin/bash

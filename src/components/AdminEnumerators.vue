e15bf5fe3ca1a3b6f49121b06d0c60fd53cb5a6e
Mike Storey <mike@ssgpp.com>
2024-02-18 17:38:20 -0500

    Adjusted admin enumerators to new structure

--- src/components/AdminEnumerators.vue
+++ src/components/AdminEnumerators.vue
@@ -1,23 +1,72 @@
 <template>
   <div>
-    <EnumeratorItem :enumerators="config.enums.statuses" :name="'Status'"/>
-    <EnumeratorItem :enumerators="config.enums.device" :name="'Devices'"/>
-    <EnumeratorItem :enumerators="config.enums.title" :name="'Titles'"/>
-    <EnumeratorItem :enumerators="config.enums.domain" :name="'Domains'"/>
-    <EnumeratorItem :enumerators="config.enums.resourceType" :name="'Resource Types'"/>
+    <ul>
+      <v-expansion-panels>
+        <div v-for="(properties, collectionName) in config.enums" :key="collectionName" class="wide">
+          <template v-if="collectionName !== '_id' && collectionName !== 'name' && collectionName !== 'status'">
+            <li class="box">
+              <v-expansion-panel>
+                <v-expansion-panel-title><h2>{{ collectionName }}</h2></v-expansion-panel-title>
+                <v-expansion-panel-text>
+                  <ul>
+                    <v-expansion-panels>
+                      <div v-for="(values, propertyName) in properties" :key="propertyName" class="wide">
+                        <li class="box">
+                          <v-expansion-panel>
+                            <v-expansion-panel-title><h3>{{ propertyName }}</h3></v-expansion-panel-title>
+                            <v-expansion-panel-text>
+                              <ul>
+                                <div>
+                                  <li v-for="(description, name) in values" :key="name" class="left">
+                                    <span class="bold">{{ name }}</span>: {{ description }}
+                                  </li>
+                                </div>
+                              </ul>
+                            </v-expansion-panel-text>
+                          </v-expansion-panel>
+                        </li>
+                      </div>
+                    </v-expansion-panels>
+                  </ul>
+                </v-expansion-panel-text>
+              </v-expansion-panel>
+            </li>
+          </template>
+        </div>
+      </v-expansion-panels>
+    </ul>
   </div>
 </template>
   
 <script>
 import { mapState } from 'vuex';
-import EnumeratorItem from './AdminEnumeratorItem.vue';
 
 export default {
-  components: {
-    EnumeratorItem
-  },  
   computed: {
     ...mapState(['config'])
   }
 };
 </script>
+
+<style scoped>
+
+.wide {
+  width: 100%;
+}
+
+.left {
+  text-align: left;
+}
+
+.bold {
+  font-weight: bold;
+}
+
+.box {
+  margin: 5px;
+  /* padding: 10px; */
+  border: 1px solid #ccc;
+  border-radius: 1px;
+
+}
+</style>

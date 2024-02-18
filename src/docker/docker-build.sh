27b7eb1ff3b0603948c7216949f1aff569163587
Mike Storey <mike@ssgpp.com>
2024-02-18 11:55:55 -0500

    Improving docker build for testing process

--- src/docker/docker-build.sh
+++ src/docker/docker-build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Ensure we are running in the proper folder
+[[ -d "./src/docker" ]] && [[ -f "./src/main.js" ]] || \
+     echo "This script must be run from the repository root folder"; exit 1
+
+# CI Will not have access to .gitignore'd files, so we remove node_modules
+rm -r ./node_modules
+
 # Build Docker Image
 docker build --file src/docker/Dockerfile --tag ghcr.io/agile-learning-institute/mentorhub-person-ui:latest .
 if [ $? -ne 0 ]; then
@@ -7,4 +14,8 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-/bin/bash <(curl -o - https://raw.githubusercontent.com/agile-learning-institute/mentorhub/main/docker-configurations/run-local.sh) person
+# Start the containers
+mh up person
+
+# Reinstall dependencies removed in the beginning of the script
+npm install

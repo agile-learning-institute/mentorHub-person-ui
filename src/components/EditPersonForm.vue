97cbe875e8a643d7b19a72c0120e004efd5495f0
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2024-03-02 12:46:52 +0100

    Add v-selectors for the enums on the EditPersonForm

--- src/components/EditPersonForm.vue
+++ src/components/EditPersonForm.vue
@@ -10,16 +10,17 @@
           <v-checkbox class="flex-item" label="Donor" @change="saveMe($event, 'donor')" v-model="person.donor"></v-checkbox>
           <v-checkbox class="flex-item" label="Contact" @change="saveMe($event, 'contact')" v-model="person.contact"></v-checkbox>
         </div>
-        <v-text-field label="Status" @change="saveMe($event, 'status')" v-model="person.status"></v-text-field>
+        <v-select label="Status" @change="saveMe($event, 'status')" v-model="person.status" :items="people.status" clearable></v-select>
         <v-text-field label="Notes" @change="saveMe($event, 'description')" v-model="person.description"></v-text-field>
-        <v-text-field label="Title" @change="saveMe($event, 'title')" v-model="person.title"></v-text-field>
+        <v-select label="Title" @change="saveMe($event, 'title')" v-model="person.title" :items="people.title" clearable></v-select>
+        <v-select label="Cadence" @change="saveMe($event, 'cadence')" v-model="person.cadence" :items="people.cadence" clearable></v-select>
         <v-text-field label="Email" @change="saveMe($event, 'eMail')" v-model="person.eMail"></v-text-field>
         <v-text-field label="GitHub" @change="saveMe($event, 'gitHub')" v-model="person.gitHub"></v-text-field>
         <v-text-field label="Phone" @change="saveMe($event, 'phone')" v-model="person.phone"></v-text-field>
-        <v-text-field label="Device" @change="saveMe($event, 'device')" v-model="person.device"></v-text-field>
+        <v-select label="Device" @change="saveMe($event, 'device')" v-model="person.device" :items="people.device" clearable></v-select>
         <v-text-field label="Location" @change="saveMe($event, 'location')" v-model="person.location"></v-text-field>
-        <v-text-field label="MentorName" @change="saveMe($event, 'mentorName')" v-model="person.mentorName"></v-text-field>
-        <v-text-field label="PartnerName" @change="saveMe($event, 'partnerName')" v-model="person.partnerName"></v-text-field>
+        <v-select label="MentorName" @change="saveMe($event, 'mentorName')" v-model="mentorName" :items="config.mentors" item-title="name" item-value="_id" clearable return-object></v-select>
+        <v-select label="PartnerName" @change="saveMe($event, 'partnerName')" v-model="partnerName" :items="config.partners" item-title="name" item-value="_id" clearable return-object></v-select>
       </v-form>
     </div>
 </template>
@@ -29,7 +30,24 @@ import { mapState } from 'vuex';
 
 export default {
   computed: {
-    ...mapState(['person'])
+    ...mapState({
+      person: state => state.person,
+      config: state => state.config,
+    }),
+    people() {
+      return this.config.enums?.people ? {
+        status: Object.keys(this.config.enums.people.status),
+        title: Object.keys(this.config.enums.people.title),
+        cadence: Object.keys(this.config.enums.people.cadence),
+        device: Object.keys(this.config.enums.people.device),
+      } : {};
+    },
+    mentorName() {
+      return this.config.mentors ? this.config.mentors.filter(mentor => mentor.ID === this.person.mentorId)[0]?.name : "";
+    },
+    partnerName() {
+      return this.config.partners ? this.config.partners.filter(partner => partner.ID === this.person.partnerId)[0]?.name : "";
+    }
   },
   mounted() {
     this.$store.dispatch('getPerson', this.$route.params.id);

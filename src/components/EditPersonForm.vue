9f12a5c8fc13c59c827c62a42b9d4b507d32c657
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2024-03-02 19:03:09 +0100

    Add validators and regex for EditPersonForm text fields

--- src/components/EditPersonForm.vue
+++ src/components/EditPersonForm.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
       <h1>Edit Person</h1>
-      <v-form>
+      <v-form v-model="form" ref="form">
         <!-- <v-text-field label="ID" v-if="mode==='edit'" v-bind:model-value="$route.params.id" v-bind:readonly="true"></v-text-field> -->
         <v-text-field label="Name" @change="saveMe($event, 'name')" v-model="person.name" required></v-text-field>
         <div class="flex-container">
@@ -11,14 +11,14 @@
           <v-checkbox class="flex-item" label="Contact" @change="saveMe($event, 'contact')" v-model="person.contact"></v-checkbox>
         </div>
         <v-select label="Status" @change="saveMe($event, 'status')" v-model="person.status" :items="people.status" clearable></v-select>
-        <v-text-field label="Notes" @change="saveMe($event, 'description')" v-model="person.description"></v-text-field>
+        <v-text-field label="Notes" @change="saveMe($event, 'description')" :rules="[rules.descriptionCount]" v-model="person.description"></v-text-field>
         <v-select label="Title" @change="saveMe($event, 'title')" v-model="person.title" :items="people.title" clearable></v-select>
         <v-select label="Cadence" @change="saveMe($event, 'cadence')" v-model="person.cadence" :items="people.cadence" clearable></v-select>
-        <v-text-field label="Email" @change="saveMe($event, 'eMail')" v-model="person.eMail"></v-text-field>
-        <v-text-field label="GitHub" @change="saveMe($event, 'gitHub')" v-model="person.gitHub"></v-text-field>
-        <v-text-field label="Phone" @change="saveMe($event, 'phone')" v-model="person.phone"></v-text-field>
+        <v-text-field label="Email" @change="saveMe($event, 'eMail')" :rules="[rules.required, rules.emailCount, rules.emailPattern]" v-model="person.eMail"></v-text-field>
+        <v-text-field label="GitHub" @change="saveMe($event, 'gitHub')" :rules="[rules.githubCount]" v-model="person.gitHub"></v-text-field>
+        <v-text-field label="Phone" @change="saveMe($event, 'phone')" :rules="[rules.required, rules.phonePattern]" v-model="person.phone"></v-text-field>
         <v-select label="Device" @change="saveMe($event, 'device')" v-model="person.device" :items="people.device" clearable></v-select>
-        <v-text-field label="Location" @change="saveMe($event, 'location')" v-model="person.location"></v-text-field>
+        <v-text-field label="Location" @change="saveMe($event, 'location')" :rules="[rules.locationCount]" v-model="person.location"></v-text-field>
         <v-select label="MentorName" @change="saveMe($event, 'mentorName')" v-model="mentorName" :items="config.mentors" item-title="name" item-value="_id" clearable return-object></v-select>
         <v-select label="PartnerName" @change="saveMe($event, 'partnerName')" v-model="partnerName" :items="config.partners" item-title="name" item-value="_id" clearable return-object></v-select>
       </v-form>
@@ -47,6 +47,21 @@ export default {
     },
     partnerName() {
       return this.config.partners ? this.config.partners.filter(partner => partner.ID === this.person.partnerId)[0]?.name : "";
+    }
+  },
+  data() {
+    return {
+      form: false,
+      rules: {
+        required: value => !!value || "This is a required field",
+        emailPattern: value => /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(value) || "Invalid email",
+        phonePattern: value => /^\d{3}[-.\s]?\d{3}[-.\s]?\d{4}$/.test(value) || "Invalid phone number",
+        nameCount: value => value?.length <= 32 || "Max 32 characters for name field",
+        descriptionCount: value => value?.length <= 256 || "Max 256 characters for description field",
+        emailCount: value => value?.length <= 256 || "Max 256 characters for email field",
+        githubCount: value => value?.length <= 32 || "Max 32 characters for github field",
+        locationCount: value => value?.length <= 64 || "Max 64 characters for location field",
+      }
     }
   },
   mounted() {

76eb73a7469bee4d5fca6152004e72c6d6b0a104
John Kluse <j.kluse@gmail.com>
2024-03-04 09:39:15 -0700

    peoplelist with vuetify components

--- src/components/PeopleList.vue
+++ src/components/PeopleList.vue
@@ -1,76 +1,89 @@
 <template>
-  <div>
-    <h1>
-      <a :href="newPersonLink()" target="_blank">Add New Person</a>
-    </h1>
-    <ul>
-      <li v-for="name in people" :key="name.ID" >
-        <div class="listItem">
-          <a :href="editPersonLink(name.ID)" target="_blank">{{name.name}}</a>  
-        </div>
-      </li>
-    </ul>
-  </div>
+	<v-container>
+		<h1 class="text-left">People</h1>
+		<div class="mb-5">
+			<a :href="newPersonLink()" target="_blank">
+				<v-btn size="large" variant="tonal" class="bg-grey-lighten-5">
+					Add New Person
+				</v-btn>
+			</a>
+		</div>
+		<v-table>
+			<thead>
+				<tr>
+					<th class="text-left">Name</th>
+					<th class="text-left">Actions</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr v-for="person in people" :key="person.ID">
+					<td class="text-left">{{ person.name }}</td>
+					<td class="text-left">
+						<v-btn :href="editPersonLink(person.ID)" target="_blank"
+							>Edit</v-btn
+						>
+					</td>
+				</tr>
+			</tbody>
+		</v-table>
+	</v-container>
 </template>
-  
+
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-  data() {
-    return {
-        people: []
-    };
-  },
-  mounted() {
-    document.title = 'People';
-    this.getData();  // Call getData() when the component is mounted
-    document.addEventListener("visibilitychange", this.handleVisibilityChange);
-  },
-  beforeDestroy() {
-    document.removeEventListener("visibilitychange", this.handleVisibilityChange);
-  },
-  methods: {
-    handleVisibilityChange() {
-      if (document.visibilityState === 'visible') {
-        this.getData(); 
-      }
-    },
-    async getData() {
-      const apiHost = process.env.VUE_APP_API_HOST;
-      const apiPort = process.env.VUE_APP_API_PORT;
-      const apiUrl = `${apiHost}:${apiPort}/api/person/`;
+	data() {
+		return {
+			people: [],
+		};
+	},
+	mounted() {
+		document.title = "People";
+		this.getData(); // Call getData() when the component is mounted
+		document.addEventListener("visibilitychange", this.handleVisibilityChange);
+	},
+	beforeDestroy() {
+		document.removeEventListener(
+			"visibilitychange",
+			this.handleVisibilityChange
+		);
+	},
+	methods: {
+		handleVisibilityChange() {
+			if (document.visibilityState === "visible") {
+				this.getData();
+			}
+		},
+		async getData() {
+			const apiHost = process.env.VUE_APP_API_HOST;
+			const apiPort = process.env.VUE_APP_API_PORT;
+			const apiUrl = `${apiHost}:${apiPort}/api/person/`;
 
-      try {
-        // create parameter to prevent cache on flat API calls
-        const parms = {params: {_: new Date().getTime()}}
-        const apiResponse = await axios.get(apiUrl, parms);
-        this.people = apiResponse.data        
-      } catch(error) {
-        console.log("Error:", error);
-        console.error('An error occurred:', error);
-      }
-    },
-    newPersonLink() {
-      return this.$router.resolve({ name: 'AddPerson'}).href;
-    },
-    editPersonLink(id) {
-      // Generate the link for EditPerson route
-      return this.$router.resolve({ name: 'EditPerson', params: { id: id } }).href;
-    },
-  },
+			try {
+				// create parameter to prevent cache on flat API calls
+				const parms = { params: { _: new Date().getTime() } };
+				const apiResponse = await axios.get(apiUrl, parms);
+				this.people = apiResponse.data;
+			} catch (error) {
+				console.log("Error:", error);
+				console.error("An error occurred:", error);
+			}
+		},
+		newPersonLink() {
+			return this.$router.resolve({ name: "AddPerson" }).href;
+		},
+		editPersonLink(id) {
+			// Generate the link for EditPerson route
+			return this.$router.resolve({ name: "EditPerson", params: { id: id } })
+				.href;
+		},
+	},
 };
-
 </script>
 
 <style scoped>
-
-.listItem {
-  width: 100%;
-  margin: 5px;
-  padding: 10px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
+tbody tr:nth-of-type(odd) {
+	background-color: rgba(0, 0, 0, 0.05);
 }
-
 </style>
